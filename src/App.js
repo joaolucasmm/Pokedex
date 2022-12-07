6693f19240370bf5924875d7b2c1e240fc4a61df
João Lucas Miranda Menegasso <joaol_mm@hotmail.com>
2022-12-06 21:11:11 -0300

    Added search engine's first version

--- src/App.js
+++ src/App.js
@@ -12,33 +12,45 @@ function App() {
 	const [pokemonName, setPokemonName] = useState('');
 	const [pokemonID, setPokemonID] = useState('1');
 	const [pokemonSprite, setPokemonSprite] = useState();
+	const [search, setSearch] = useState();
+	const [status, setStatus] = useState(404);
 	
 	// API Call
 	const fetchPokemon = async (pokemon) => {
 		const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+		
+		setStatus(APIResponse.status);
+
 		const data = await APIResponse.json();
 		return data;
+		// if(status === 200) {
+		// }
 	}
 	
 	// Rendering pokemon info
 	const renderPokemon = async (pokemon) => {
+
+		// setPokemonName('Loading...');
+
 		const data = await fetchPokemon(pokemon);
+		// console.log(status);
+
 		const PokeName = data.name;
-		console.log(data);
 		setPokemonName(PokeName);
 		setPokemonID(data.id);
 		setPokemonSprite(data.sprites.versions['generation-v']['black-white'].animated.front_default);
+		// if(status === 200) {
+		// }
 	}
-	console.log(pokemonSprite);
-
+	
 	renderPokemon(pokemonID);
-
+	
 	return (
 		
 		<div className='app_window pokedex_page'>
             <img src={pokedex} alt='Pokedex' className='pokedex_image'/>
 			<Pokedex className='pokedex_element' pokemonNumber={pokemonID} pokemonName={pokemonName} pokeSprite={pokemonSprite}/>
-			<Search />
+			<Search setId={setPokemonID} setSearch={setSearch} status={status} />
 			<Button className='button_element' actualPoke={pokemonID} updatePoke={setPokemonID} />
 		</div>
   	);
--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 
-function Search() {
+function Search({ setId, setSearch, status }) {
+    const searchPokemon = function (e) {
+        e.preventDefault();
+        // console.log(status);
+        setId(e.target.search.value.toLowerCase());
+        // if(status === 200) {
+        // }
+        e.target.search.value =  '';
+    }
     return (
-        <form className='form'>
+        <form className='form' onSubmit={searchPokemon}>
             <input 
                 type="search"
+                name="search"
                 className='input_search'
                 placeholder='Name or Number'
                 required
